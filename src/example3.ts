6d383746ae450472a5b0d259ac93558bbac078c3
Nicolás Ramos <jnicolas.ramos10@gmail.com>
2024-09-09 18:28:37 -0300

    new examples

---
+++ src/example3.ts
@@ -0,0 +1,7 @@
+/* 
+Ejercicio 3: Lista de Tareas
+Crea una clase Tarea con las propiedades titulo y completada. Luego, usa un array de tareas y filter para obtener solo las tareas que no han sido completadas.
+
+
+Ejercicio 4: Biblioteca de Libros
+Crea una clase Libro con las propiedades titulo y autor. Luego, crea un array de libros y usa map para generar un nuevo array que contenga solo los títulos de los libros. */
